6f78280ef18280a0a6564a1b198a997d0514222a
Zach Mitchell <zmitchell@fastmail.com>
2023-11-10 15:58:18 -0700

    feat: configure shell and utils

--- stacks/system.nix
+++ stacks/system.nix
@@ -6,20 +6,58 @@
 
 {
   system.stateVersion = "23.05";
+  nixpkgs.config.allowUnfree = true;
 
+  # Packages that should be available to all users (including the root user)
   environment.systemPackages = with pkgs; [
     gitFull
     neovim
-    hello
+    just
+    ripgrep
+    bat
+    fd
+    zoxide
+    atuin
+    fzf
+    tealdeer
+    starship
+    jq
   ];
 
+  # Enable the OpenSSH server
   services.openssh.enable = true;
-  programs.zsh.enable = true;
 
+  # Configure Zsh and its plugins for use as the default shell
+  programs.zsh.enable = true;
+  programs.zsh.syntaxHighlighting.enable = true;
+  programs.zsh.enableCompletion = true;
+  programs.zsh.autosuggestions.enable = true;
+  programs.starship.enable = true;
+  programs.starship.settings = {
+    format = "\
+    $username\
+    $hostname\
+    $directory\
+    $git_branch\
+    $git_state\
+    $git_status\
+    $python\
+    $cmd_duration\
+    $line_break\
+    $nix_shell\
+    $character
+    ";
+    command_timeout = 3000;
+    nix_shell.heuristic = true;
+    directory.truncate_to_repo = false;
+  };
+  
+  # Enable flakes
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
 
+  # Miscellaneous files we want to appear on the system
   environment.etc = {
     # Store the flake that built the system
     sourceFlake.source = builtins.path {
