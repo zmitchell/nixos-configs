774ec0b866a00c147956ffce5605dd0aecc23608
Zach Mitchell <zmitchell@fastmail.com>
2024-02-10 23:34:34 +0000

    feat: config for vm-disko

--- flake.nix
+++ flake.nix
@@ -7,36 +7,73 @@
   # For fixing the 'command-not-found' script that's broken on flake-based systems
   inputs.flake-programs-sqlite.url = "github:wamserma/flake-programs-sqlite";
   inputs.flake-programs-sqlite.inputs.nixpkgs.follows = "nixpkgs";
+  # Declarative filesystem setup
+  inputs.disko.url = "github:nix-community/disko";
+  inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs =
-    inputs@{ self, nixpkgs, nix-index-database, flake-programs-sqlite, ... }:
+    inputs@{ self, nixpkgs, nix-index-database, flake-programs-sqlite, disko, ... }:
     let
-      vmConfig = let pkgs = nixpkgs.legacyPackages.aarch64-linux;
-      in {
-        system = "aarch64-linux";
-        specialArgs = { inherit inputs pkgs; };
-        modules = [
-          {
-            virtualisation.vmware.guest.enable = true;
-            imports = [ ./hardware/vm.nix ];
-          }
-          {
-            networking.hostName = "nixos";
-            networking.domain = "vms.home";
-          }
-          ./stacks/boot.nix
-          ./stacks/system.nix
-          ./stacks/users.nix
-          ./stacks/shell.nix
-          ./stacks/git.nix
-          nix-index-database.nixosModules.nix-index
-          flake-programs-sqlite.nixosModules.programs-sqlite
-        ];
-      };
+      vmConfig = 
+        let
+          pkgs = nixpkgs.legacyPackages.aarch64-linux;
+        in {
+          system = "aarch64-linux";
+          specialArgs = { inherit inputs pkgs; };
+          modules = [
+            {
+              virtualisation.vmware.guest.enable = true;
+              imports = [ ./hardware/vm.nix ];
+            }
+            {
+              networking.hostName = "nixos";
+              networking.domain = "vms.home";
+              networking.hostId = "00042069";
+            }
+            ./stacks/boot.nix
+            ./stacks/system.nix
+            ./stacks/users.nix
+            ./stacks/shell.nix
+            ./stacks/git.nix
+            nix-index-database.nixosModules.nix-index
+            flake-programs-sqlite.nixosModules.programs-sqlite
+          ];
+        };
+      vmConfigDisko = 
+        let
+          pkgs = nixpkgs.legacyPackages.aarch64-linux;
+        in {
+          system = "aarch64-linux";
+          specialArgs = { inherit inputs pkgs; };
+          modules = [
+            {
+              virtualisation.vmware.guest.enable = true;
+              imports = [ ./hardware/vm-disko.nix ];
+            }
+            {
+              networking.hostName = "nixos";
+              networking.domain = "vms.home";
+              networking.hostId = "10042069";
+            }
+            ./stacks/boot.nix
+            ./stacks/system.nix
+            ./stacks/users.nix
+            ./stacks/shell.nix
+            ./stacks/git.nix
+            nix-index-database.nixosModules.nix-index
+            flake-programs-sqlite.nixosModules.programs-sqlite
+            (import ./stacks/zfs_single_drive.nix {
+              device = "/dev/nvme0n1";
+              user = "zmitchell";
+            })
+            disko.nixosModules.disko
+          ];
+        };
     in {
-      nixosModules = { inherit vmConfig; };
+      nixosModules = { inherit vmConfig vmConfigDisko; };
       nixosConfigurations = {
         vm = nixpkgs.lib.nixosSystem self.nixosModules.vmConfig;
+        vm-disko = nixpkgs.lib.nixosSystem self.nixosModules.vmConfigDisko;
       };
     };
 }
--- stacks/disko_scripts.nix
+++ stacks/disko_scripts.nix
@@ -1,11 +1,14 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = [
-    (pkgs.runCommandCC "disko-scripts" {} ''
-      mkdir -p $out/bin
-      ln -s ${config.system.build.diskoScript} $out/bin/disko-script
-      ln -s ${config.system.build.formatScript} $out/bin/disko-format
-      ln -s ${config.system.build.mountScript} $out/bin/mount-script
-      '')
+    # (pkgs.runCommandCC "disko-scripts" {} ''
+    #   mkdir -p $out/bin
+    #   ln -s ${config.system.build.diskoScript} $out/bin/disko-script
+    #   ln -s ${config.system.build.formatScript} $out/bin/disko-format
+    #   ln -s ${config.system.build.mountScript} $out/bin/mount-script
+    #   '')
+     config.system.build.diskoScript
+     config.system.build.formatScript
+     config.system.build.mountScript
   ];
 }
