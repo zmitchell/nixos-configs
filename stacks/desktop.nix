de59a987fb8fc08305c2c86839257b7b276ec4af
Zach Mitchell <zmitchell@fastmail.com>
2024-02-25 15:20:47 -0700

    feat: add desktop apps

--- stacks/desktop.nix
+++ stacks/desktop.nix
@@ -1,6 +1,22 @@
-{...}:
+{pkgs, ...}:
 {
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  programs.gnome-disks.enable = true;
+  programs.gnome-terminal.enable = true;
+  users.users.zmitchell.packages = with pkgs; [
+    firefox
+    gnome.evince # document viewer
+    gnome.gedit # text editor
+    gnome.eog # image viewer
+    gnome.sushi # quick preview for nautilus
+    # For game streaming
+    moonlight-qt
+  ];
+
+  # Gaming related stuff
+  hardware.steam-hardware.enable = true;
+  programs.steam.enable = true;
 }
