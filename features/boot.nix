db174451f5905d2f232b4a333ab5158daebb20fe
Zach Mitchell <zmitchell@fastmail.com>
2024-03-19 22:55:36 +0000

    feat(boot): tweak boot settings

--- features/boot.nix
+++ features/boot.nix
@@ -2,5 +2,5 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
   boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 100;
+  boot.loader.systemd-boot.configurationLimit = 50;
 }
--- hosts/thiccboi.nix
+++ hosts/thiccboi.nix
@@ -15,6 +15,9 @@ in
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
+  # Extra boot settings
+  boot.loader.timeout = 0; # we have scripts for booting
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
