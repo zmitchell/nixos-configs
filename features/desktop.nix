777eade8a1c3c89eabcc394fc74f1871cec45d3a
Zach Mitchell <zmitchell@fastmail.com>
2024-03-21 05:00:36 +0000

    feat: don't let the system sleep

--- features/desktop.nix
+++ features/desktop.nix
@@ -44,4 +44,10 @@
       ];
     })
   ];
+
+  # Don't let the system sleep, it's a server
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
 }
