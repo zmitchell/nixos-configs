05cc749282f19b038684f52b80a8dc74e37a464a
Zach Mitchell <zmitchell@fastmail.com>
2024-06-09 11:29:23 -0600

    fix: boot settings

--- modules/base_system.nix
+++ modules/base_system.nix
@@ -45,5 +45,10 @@
         path = inputs.self;
       };
     };
+
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.efi.efiSysMountPoint = "/boot";
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.systemd-boot.configurationLimit = 50;
   };
 }
