3b3fe2a348df2a296c3bae0740ab13d607c6de72
Zach Mitchell <zmitchell@fastmail.com>
2024-07-02 20:20:15 -0600

    fix: don't look for ssh keys in home

--- features/desktop_generic.nix
+++ features/desktop_generic.nix
@@ -3,7 +3,6 @@
   environment.systemPackages = with pkgs; [
     kitty
     firefox
-    nvtop
   ];
 
   # Fonts
--- features/system.nix
+++ features/system.nix
@@ -24,11 +24,13 @@
   # Enable the OpenSSH server
   services.openssh = {
     enable = true;
+    authorizedKeysInHomedir = false;
     settings = {
       # Forbid root login through SSH.
       PermitRootLogin = "no";
       # Use keys only. Remove if you want to SSH using password (not recommended)
       PasswordAuthentication = false;
+      LogLevel="DEBUG";
     };
   };
   services.tailscale.enable = true;
