b389650dea0f8debc9be58f0079348fe03c38286
Zach Mitchell <zmitchell@fastmail.com>
2024-11-25 12:21:27 -0700

    chore(chungus): WIP wrapped bpftrace

--- homeConfigurations/chungus.nix
+++ homeConfigurations/chungus.nix
@@ -1,11 +1,27 @@
 {pkgs, lib, inputs, user, ...}:
 let
   shellAliases = import ./shell-aliases.nix;
+  # bpftrace-wrapped = pkgs.runCommand "bpftrace-with-kernel-source" {
+  #   # inherit (pkgs.bpftrace) pname version meta outputs postInstall;
+  #   # inherit (pkgs.bpftrace) postInstall;
+  #   nativeBuildInputs = [ pkgs.makeBinaryWrapper ];
+  #   propagatedBuildInputs = [ pkgs.linuxHeaders ];
+  # } ''
+  #   mkdir $out
+  #   cp -rs --no-preserve=mode,ownership ${pkgs.bpftrace.out}/* $out/
+  #   rm -rf $out/bin/bpftrace
+  #   makeWrapper ${pkgs.bpftrace}/bin/bpftrace $out/bin/bpftrace --set BPFTRACE_KERNEL_SOURCE ${pkgs.linuxHeaders} 
+  #   find ${pkgs.bpftrace}/bin -t f -name '*.bt' 
+  #   # mkdir $man
+  #   # cp -R ${pkgs.bpftrace.man}/* $man/
+  # '';
 in
 {
   imports = [
     ./common.nix
   ];
+
+  # home.packages = [ bpftrace-wrapped ];
 
   # Configure zsh so it's not terrible when we need to use it
   programs.zsh = {
