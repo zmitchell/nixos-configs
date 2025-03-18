6528bb28f88f5c6f9329003beb2bc85f3b640233
Zach Mitchell <zmitchell@fastmail.com>
2025-03-18 12:02:02 -0600

    chore: remove unused config

--- homeConfigurations/chungus.nix
+++ homeConfigurations/chungus.nix
@@ -1,34 +1,17 @@
 {pkgs, lib, inputs, user, ...}:
 let
   shellAliases = import ./shell-aliases.nix;
-  # bpftrace-wrapped = pkgs.runCommand "bpftrace-with-kernel-source" {
-  #   # inherit (pkgs.bpftrace) pname version meta outputs postInstall;
-  #   # inherit (pkgs.bpftrace) postInstall;
-  #   nativeBuildInputs = [ pkgs.makeBinaryWrapper ];
-  #   propagatedBuildInputs = [ pkgs.linuxHeaders ];
-  # } ''
-  #   mkdir $out
-  #   cp -rs --no-preserve=mode,ownership ${pkgs.bpftrace.out}/* $out/
-  #   rm -rf $out/bin/bpftrace
-  #   makeWrapper ${pkgs.bpftrace}/bin/bpftrace $out/bin/bpftrace --set BPFTRACE_KERNEL_SOURCE ${pkgs.linuxHeaders} 
-  #   find ${pkgs.bpftrace}/bin -t f -name '*.bt' 
-  #   # mkdir $man
-  #   # cp -R ${pkgs.bpftrace.man}/* $man/
-  # '';
 in
 {
   imports = [
     ./common.nix
   ];
 
-  home.packages = with pkgs; [ tcsh ];
   dconf.settings = {  
     "org/gnome/settings-daemon/plugins/power" = {
       sleep-inactive-ac-type = "nothing";
     };
   };
-
-  # home.packages = [ bpftrace-wrapped ];
 
   # Configure zsh so it's not terrible when we need to use it
   programs.zsh = {
