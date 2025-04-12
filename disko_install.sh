a82f9546659dc6a3d9bf8ac995005b64550adb41
Zach Mitchell <zmitchell@fastmail.com>
2025-04-11 20:38:57 -0600

    chore: tweak disko script

--- disko_install.sh
+++ disko_install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env
 
-sudo nix run 'github:nix-community/disko/latest#disko-install' -- --write-efi-boot-entries --flake .#slim --disk main /dev/nvme0n1
+sudo nix run 'github:nix-community/disko/latest#disko-install' --extra-experimental-features "nix-command flakes" -- --write-efi-boot-entries --flake .#slim --disk main /dev/nvme0n1
